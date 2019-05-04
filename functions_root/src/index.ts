8d0edb1ac7dc9be7d15b9d71f2cbdeea9d1b3008
narumincho <narumincho.starfy@gmail.com>
2019-05-04 10:25:52 +0900

    GraphQLのお試し

---
+++ functions_root/src/index.ts
@@ -0,0 +1,82 @@
+import * as functions from "firebase-functions"
+import * as admin from "firebase-admin"
+import * as graphql from "graphql"
+import * as graphalExpress from "express-graphql"
+import { Firestore } from "@google-cloud/firestore";
+
+admin.initializeApp();
+const dataBase = admin.firestore();
+const dataBaseCollection = dataBase.collection("stringDataBase");
+
+
+console.log("サーバーのプログラムが読み込まれたぜ")
+
+/** データベースで保存するデータの形式を決めるスキーマ */
+const schema = new graphql.GraphQLSchema({
+    query: new graphql.GraphQLObjectType({
+        name: "RootQueryType",
+        description: "RootQueryTypeの説明。たぶんルートのクエリの型なんだと思う",
+        fields: {
+            "hello": {
+                type: graphql.GraphQLNonNull(graphql.GraphQLString),
+                resolve: () => "hello world!",
+                description: "世界に挨拶する"
+            },
+            "join": {
+                type: graphql.GraphQLNonNull(graphql.GraphQLString),
+                resolve: (source, { text }, context, info) => {
+                    return "textは" + text
+                },
+                args: {
+                    "text": {
+                        type: graphql.GraphQLString,
+                        description: "結合する対象の文字列",
+                        defaultValue: "defaultValue"
+                    }
+                },
+                description: "指定した文字列に\"textは\"を頭につける。うまくいくかどうかわからない"
+            },
+            "setData": {
+                type: new graphql.GraphQLObjectType({
+                    name: "stringDataBase",
+                    fields: {
+                        "id": {
+                            type: graphql.GraphQLID,
+                            description: "ランダムに生成されるID"
+                        },
+                        "text": {
+                            type: graphql.GraphQLString,
+                            description: "前後の空白が取り除かれたテキスト"
+                        },
+                        "createdAt": {
+                            type: graphql.GraphQLFloat,
+                            description: "投稿された日付。JSのnew Date().getTime()で取得できるようなUNIX timeにミリ秒の情報を加えた(x1000した)もの"
+                        }
+                    }
+                }),
+                resolve: async (source, args, context, info) => {
+                    const text: string = args.text
+                    const documentReference = await dataBaseCollection.add({
+                        "text": text.trim(),
+                        "createdAt": admin.firestore.FieldValue.serverTimestamp()
+                    })
+                    const documentData = (await documentReference.get()).data() as { text: string, date: FirebaseFirestore.Timestamp }
+                    return {
+                        "id": documentReference.id,
+                        "text": documentData.text,
+                        "createdAt": documentData.date.toMillis()
+                    }
+                },
+                args: {
+                    "text": {
+                        type: graphql.GraphQLNonNull(graphql.GraphQLString),
+                        description: "設定する文字列"
+                    }
+                },
+                description: "データベースに文字列をアップロードする"
+            }
+        }
+    })
+})
+
+export const api = functions.https.onRequest(graphalExpress({ schema: schema, graphiql: true }));
