5e517b0c8e782186b0e584734a81eff1b3793fe1
narumincho <narumincho.starfy@gmail.com>
2019-12-16 16:36:05 +0900

    パスの区切り文字を/にした

--- script/deploy.ps1
+++ script/deploy.ps1
@@ -2,22 +2,22 @@ $Host.UI.RawUI.ForegroundColor = "Yellow";
 Write-Output "Compile Hosting Files And Upload Firebase Server";
 
 Write-Output "Delete Dist Folder";
-Remove-Item .\distribution -Recurse;
+Remove-Item ./distribution -Recurse;
 
 $Host.UI.RawUI.ForegroundColor = "Yellow";
 Write-Output "Compile Elm And Minify ...";
 $Host.UI.RawUI.ForegroundColor = "Gray";
 
-.\node_modules\.bin\parcel.ps1 build .\source\call\call.ts --out-dir distribution --out-file main.js
+./node_modules/.bin/parcel.ps1 build ./source/call/call.ts --out-dir distribution --out-file main.js
 
 # parcelのパスのバグとスコープの問題を解決するために置換する
-(Get-Content .\distribution\main.js).Replace("navigator.serviceWorker.register(""/__\serviceworker.js", "navigator.serviceWorker.register(""/serviceworker.js") | Set-Content .\distribution\main.js
+(Get-Content ./distribution/main.js).Replace("navigator.serviceWorker.register(""/__\serviceworker.js", "navigator.serviceWorker.register(""/serviceworker.js") | Set-Content ./distribution/main.js
 
-Move-Item .\distribution\__\serviceworker.js .\distribution\
-Move-Item .\distribution\__\serviceworker.js.map .\distribution\
-Remove-Item .\distribution\__
+Move-Item ./distribution/__/serviceworker.js ./distribution/
+Move-Item ./distribution/__/serviceworker.js.map ./distribution/
+Remove-Item ./distribution/__
 
-Copy-Item .\source\assets .\distribution -Recurse
+Copy-Item ./source/assets ./distribution -Recurse
 
 Write-Output "Upload to Firebase ...";
 $Host.UI.RawUI.ForegroundColor = "Gray";
