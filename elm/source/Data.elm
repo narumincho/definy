ee980338471acfc05a45984d365ab741afdd7a06
narumincho <narumincho.starfy@gmail.com>
2020-03-15 10:45:59 +0900

    update definy-common 0.1.18

--- elm/source/Data.elm
+++ elm/source/Data.elm
@@ -1,4 +1,4 @@
-module Data exposing (AccessToken(..), ClientMode(..), DateTime, FileHash(..), IdeaId(..), Language(..), Location(..), OpenIdConnectProvider(..), ProjectId(..), RequestLogInUrlRequestData, UrlData, UserId(..), UserPublic, accessTokenJsonDecoder, accessTokenToJsonValue, clientModeJsonDecoder, clientModeToJsonValue, dateTimeJsonDecoder, dateTimeToJsonValue, fileHashJsonDecoder, fileHashToJsonValue, ideaIdJsonDecoder, ideaIdToJsonValue, languageJsonDecoder, languageToJsonValue, locationJsonDecoder, locationToJsonValue, maybeJsonDecoder, maybeToJsonValue, openIdConnectProviderJsonDecoder, openIdConnectProviderToJsonValue, projectIdJsonDecoder, projectIdToJsonValue, requestLogInUrlRequestDataJsonDecoder, requestLogInUrlRequestDataToJsonValue, resultJsonDecoder, resultToJsonValue, urlDataJsonDecoder, urlDataToJsonValue, userIdJsonDecoder, userIdToJsonValue, userPublicJsonDecoder, userPublicToJsonValue)
+module Data exposing (AccessToken(..), Change(..), ClientMode(..), DateTime, FileHash(..), Idea, IdeaComment(..), IdeaCommentText, IdeaId(..), Language(..), Location(..), ModuleSnapshot, OpenIdConnectProvider(..), PartId(..), PartSnapshot, Project, ProjectId(..), RequestLogInUrlRequestData, Suggestion, TypeSnapshot, UrlData, UserId(..), UserPublic, UserPublicAndUserId, accessTokenJsonDecoder, accessTokenToJsonValue, changeJsonDecoder, changeToJsonValue, clientModeJsonDecoder, clientModeToJsonValue, dateTimeJsonDecoder, dateTimeToJsonValue, fileHashJsonDecoder, fileHashToJsonValue, ideaCommentJsonDecoder, ideaCommentTextJsonDecoder, ideaCommentTextToJsonValue, ideaCommentToJsonValue, ideaIdJsonDecoder, ideaIdToJsonValue, ideaJsonDecoder, ideaToJsonValue, languageJsonDecoder, languageToJsonValue, locationJsonDecoder, locationToJsonValue, maybeJsonDecoder, maybeToJsonValue, moduleSnapshotJsonDecoder, moduleSnapshotToJsonValue, openIdConnectProviderJsonDecoder, openIdConnectProviderToJsonValue, partIdJsonDecoder, partIdToJsonValue, partSnapshotJsonDecoder, partSnapshotToJsonValue, projectIdJsonDecoder, projectIdToJsonValue, projectJsonDecoder, projectToJsonValue, requestLogInUrlRequestDataJsonDecoder, requestLogInUrlRequestDataToJsonValue, resultJsonDecoder, resultToJsonValue, suggestionJsonDecoder, suggestionToJsonValue, typeSnapshotJsonDecoder, typeSnapshotToJsonValue, urlDataJsonDecoder, urlDataToJsonValue, userIdJsonDecoder, userIdToJsonValue, userPublicAndUserIdJsonDecoder, userPublicAndUserIdToJsonValue, userPublicJsonDecoder, userPublicToJsonValue)
 
 import Json.Decode as Jd
 import Json.Decode.Pipeline as Jdp
@@ -14,8 +14,8 @@ type alias DateTime =
 {-| デバッグの状態と, デバッグ時ならアクセスしているポート番号
 -}
 type ClientMode
-    = DebugMode Int
-    | Release
+    = ClientModeDebugMode Int
+    | ClientModeRelease
 
 
 {-| ログインのURLを発行するために必要なデータ
@@ -27,8 +27,8 @@ type alias RequestLogInUrlRequestData =
 {-| プロバイダー (例: LINE, Google, GitHub)
 -}
 type OpenIdConnectProvider
-    = Google
-    | GitHub
+    = OpenIdConnectProviderGoogle
+    | OpenIdConnectProviderGitHub
 
 
 {-| デバッグモードかどうか,言語とページの場所. URLとして表現されるデータ. Googleなどの検索エンジンの都合( <https://support.google.com/webmasters/answer/182192?hl=ja> )で,URLにページの言語のを入れて,言語ごとに別のURLである必要がある. デバッグ時のホスト名は <http://[::1]> になる
@@ -40,23 +40,84 @@ type alias UrlData =
 {-| 英語,日本語,エスペラント語などの言語
 -}
 type Language
-    = Japanese
-    | English
-    | Esperanto
+    = LanguageJapanese
+    | LanguageEnglish
+    | LanguageEsperanto
 
 
 {-| DefinyWebアプリ内での場所を示すもの. URLから求められる. URLに変換できる
 -}
 type Location
-    = Home
-    | User UserId
-    | Project ProjectId
+    = LocationHome
+    | LocationUser UserId
+    | LocationProject ProjectId
 
 
 {-| ユーザーが公開している情報
 -}
 type alias UserPublic =
     { name : String, imageHash : FileHash, introduction : String, createdAt : DateTime, likedProjectIdList : List ProjectId, developedProjectIdList : List ProjectId, commentedIdeaIdList : List IdeaId }
+
+
+{-| 最初に自分の情報を得るときに返ってくるデータ
+-}
+type alias UserPublicAndUserId =
+    { userId : UserId, userPublic : UserPublic }
+
+
+{-| プロジェクト
+-}
+type alias Project =
+    { name : String, icon : FileHash, image : FileHash, createdAt : DateTime }
+
+
+{-| アイデア
+-}
+type alias Idea =
+    { name : String, createdAt : DateTime, commentList : List IdeaComment, draftCommitIdList : List Suggestion }
+
+
+{-| アイデアのコメント
+-}
+type IdeaComment
+    = IdeaCommentText IdeaCommentText
+    | IdeaCommentSuggestion Suggestion
+
+
+{-| 文章でのコメント
+-}
+type alias IdeaCommentText =
+    { body : String, createdBy : UserId, createdAt : DateTime }
+
+
+{-| 編集提案
+-}
+type alias Suggestion =
+    { createdAt : DateTime, description : String, change : Change }
+
+
+{-| 変更点
+-}
+type Change
+    = ChangeProjectName String
+
+
+{-| モジュールのスナップショット
+-}
+type alias ModuleSnapshot =
+    { name : List String, description : String, export : Bool }
+
+
+{-| 型のスナップショット
+-}
+type alias TypeSnapshot =
+    { name : String, parentList : List PartId, description : String }
+
+
+{-| パーツのスナップショット
+-}
+type alias PartSnapshot =
+    { name : String, parentList : List PartId, description : String }
 
 
 type AccessToken
@@ -71,12 +132,16 @@ type ProjectId
     = ProjectId String
 
 
+type FileHash
+    = FileHash String
+
+
 type IdeaId
     = IdeaId String
 
 
-type FileHash
-    = FileHash String
+type PartId
+    = PartId String
 
 
 maybeToJsonValue : (a -> Je.Value) -> Maybe a -> Je.Value
@@ -114,13 +179,18 @@ projectIdToJsonValue (ProjectId string) =
     Je.string string
 
 
+fileHashToJsonValue : FileHash -> Je.Value
+fileHashToJsonValue (FileHash string) =
+    Je.string string
+
+
 ideaIdToJsonValue : IdeaId -> Je.Value
 ideaIdToJsonValue (IdeaId string) =
     Je.string string
 
 
-fileHashToJsonValue : FileHash -> Je.Value
-fileHashToJsonValue (FileHash string) =
+partIdToJsonValue : PartId -> Je.Value
+partIdToJsonValue (PartId string) =
     Je.string string
 
 
@@ -143,10 +213,10 @@ dateTimeToJsonValue dateTime =
 clientModeToJsonValue : ClientMode -> Je.Value
 clientModeToJsonValue clientMode =
     case clientMode of
-        DebugMode parameter ->
+        ClientModeDebugMode parameter ->
             Je.object [ ( "_", Je.string "DebugMode" ), ( "int32", Je.int parameter ) ]
 
-        Release ->
+        ClientModeRelease ->
             Je.object [ ( "_", Je.string "Release" ) ]
 
 
@@ -165,10 +235,10 @@ requestLogInUrlRequestDataToJsonValue requestLogInUrlRequestData =
 openIdConnectProviderToJsonValue : OpenIdConnectProvider -> Je.Value
 openIdConnectProviderToJsonValue openIdConnectProvider =
     case openIdConnectProvider of
-        Google ->
+        OpenIdConnectProviderGoogle ->
             Je.string "Google"
 
-        GitHub ->
+        OpenIdConnectProviderGitHub ->
             Je.string "GitHub"
 
 
@@ -189,13 +259,13 @@ urlDataToJsonValue urlData =
 languageToJsonValue : Language -> Je.Value
 languageToJsonValue language =
     case language of
-        Japanese ->
+        LanguageJapanese ->
             Je.string "Japanese"
 
-        English ->
+        LanguageEnglish ->
             Je.string "English"
 
-        Esperanto ->
+        LanguageEsperanto ->
             Je.string "Esperanto"
 
 
@@ -204,13 +274,13 @@ languageToJsonValue language =
 locationToJsonValue : Location -> Je.Value
 locationToJsonValue location =
     case location of
-        Home ->
+        LocationHome ->
             Je.object [ ( "_", Je.string "Home" ) ]
 
-        User parameter ->
+        LocationUser parameter ->
             Je.object [ ( "_", Je.string "User" ), ( "userId", userIdToJsonValue parameter ) ]
 
-        Project parameter ->
+        LocationProject parameter ->
             Je.object [ ( "_", Je.string "Project" ), ( "projectId", projectIdToJsonValue parameter ) ]
 
 
@@ -226,6 +296,116 @@ userPublicToJsonValue userPublic =
         , ( "likedProjectIdList", Je.list projectIdToJsonValue userPublic.likedProjectIdList )
         , ( "developedProjectIdList", Je.list projectIdToJsonValue userPublic.developedProjectIdList )
         , ( "commentedIdeaIdList", Je.list ideaIdToJsonValue userPublic.commentedIdeaIdList )
+        ]
+
+
+{-| UserPublicAndUserIdのJSONへのエンコーダ
+-}
+userPublicAndUserIdToJsonValue : UserPublicAndUserId -> Je.Value
+userPublicAndUserIdToJsonValue userPublicAndUserId =
+    Je.object
+        [ ( "userId", userIdToJsonValue userPublicAndUserId.userId )
+        , ( "userPublic", userPublicToJsonValue userPublicAndUserId.userPublic )
+        ]
+
+
+{-| ProjectのJSONへのエンコーダ
+-}
+projectToJsonValue : Project -> Je.Value
+projectToJsonValue project =
+    Je.object
+        [ ( "name", Je.string project.name )
+        , ( "icon", fileHashToJsonValue project.icon )
+        , ( "image", fileHashToJsonValue project.image )
+        , ( "createdAt", dateTimeToJsonValue project.createdAt )
+        ]
+
+
+{-| IdeaのJSONへのエンコーダ
+-}
+ideaToJsonValue : Idea -> Je.Value
+ideaToJsonValue idea =
+    Je.object
+        [ ( "name", Je.string idea.name )
+        , ( "createdAt", dateTimeToJsonValue idea.createdAt )
+        , ( "commentList", Je.list ideaCommentToJsonValue idea.commentList )
+        , ( "draftCommitIdList", Je.list suggestionToJsonValue idea.draftCommitIdList )
+        ]
+
+
+{-| IdeaCommentのJSONへのエンコーダ
+-}
+ideaCommentToJsonValue : IdeaComment -> Je.Value
+ideaCommentToJsonValue ideaComment =
+    case ideaComment of
+        IdeaCommentText parameter ->
+            Je.object [ ( "_", Je.string "Text" ), ( "ideaCommentText", ideaCommentTextToJsonValue parameter ) ]
+
+        IdeaCommentSuggestion parameter ->
+            Je.object [ ( "_", Je.string "Suggestion" ), ( "suggestion", suggestionToJsonValue parameter ) ]
+
+
+{-| IdeaCommentTextのJSONへのエンコーダ
+-}
+ideaCommentTextToJsonValue : IdeaCommentText -> Je.Value
+ideaCommentTextToJsonValue ideaCommentText =
+    Je.object
+        [ ( "body", Je.string ideaCommentText.body )
+        , ( "createdBy", userIdToJsonValue ideaCommentText.createdBy )
+        , ( "createdAt", dateTimeToJsonValue ideaCommentText.createdAt )
+        ]
+
+
+{-| SuggestionのJSONへのエンコーダ
+-}
+suggestionToJsonValue : Suggestion -> Je.Value
+suggestionToJsonValue suggestion =
+    Je.object
+        [ ( "createdAt", dateTimeToJsonValue suggestion.createdAt )
+        , ( "description", Je.string suggestion.description )
+        , ( "change", changeToJsonValue suggestion.change )
+        ]
+
+
+{-| ChangeのJSONへのエンコーダ
+-}
+changeToJsonValue : Change -> Je.Value
+changeToJsonValue change =
+    case change of
+        ChangeProjectName parameter ->
+            Je.object [ ( "_", Je.string "ProjectName" ), ( "string_", Je.string parameter ) ]
+
+
+{-| ModuleSnapshotのJSONへのエンコーダ
+-}
+moduleSnapshotToJsonValue : ModuleSnapshot -> Je.Value
+moduleSnapshotToJsonValue moduleSnapshot =
+    Je.object
+        [ ( "name", Je.list Je.string moduleSnapshot.name )
+        , ( "description", Je.string moduleSnapshot.description )
+        , ( "export", Je.bool moduleSnapshot.export )
+        ]
+
+
+{-| TypeSnapshotのJSONへのエンコーダ
+-}
+typeSnapshotToJsonValue : TypeSnapshot -> Je.Value
+typeSnapshotToJsonValue typeSnapshot =
+    Je.object
+        [ ( "name", Je.string typeSnapshot.name )
+        , ( "parentList", Je.list partIdToJsonValue typeSnapshot.parentList )
+        , ( "description", Je.string typeSnapshot.description )
+        ]
+
+
+{-| PartSnapshotのJSONへのエンコーダ
+-}
+partSnapshotToJsonValue : PartSnapshot -> Je.Value
+partSnapshotToJsonValue partSnapshot =
+    Je.object
+        [ ( "name", Je.string partSnapshot.name )
+        , ( "parentList", Je.list partIdToJsonValue partSnapshot.parentList )
+        , ( "description", Je.string partSnapshot.description )
         ]
 
 
@@ -278,14 +458,19 @@ projectIdJsonDecoder =
     Jd.map ProjectId Jd.string
 
 
+fileHashJsonDecoder : Jd.Decoder FileHash
+fileHashJsonDecoder =
+    Jd.map FileHash Jd.string
+
+
 ideaIdJsonDecoder : Jd.Decoder IdeaId
 ideaIdJsonDecoder =
     Jd.map IdeaId Jd.string
 
 
-fileHashJsonDecoder : Jd.Decoder FileHash
-fileHashJsonDecoder =
-    Jd.map FileHash Jd.string
+partIdJsonDecoder : Jd.Decoder PartId
+partIdJsonDecoder =
+    Jd.map PartId Jd.string
 
 
 {-| DateTimeのJSON Decoder
@@ -319,10 +504,10 @@ clientModeJsonDecoder =
             (\tag ->
                 case tag of
                     "DebugMode" ->
-                        Jd.field "int32" Jd.int |> Jd.map DebugMode
+                        Jd.field "int32" Jd.int |> Jd.map ClientModeDebugMode
 
                     "Release" ->
-                        Jd.succeed Release
+                        Jd.succeed ClientModeRelease
 
                     _ ->
                         Jd.fail ("ClientModeで不明なタグを受けたとった tag=" ++ tag)
@@ -352,10 +537,10 @@ openIdConnectProviderJsonDecoder =
             (\tag ->
                 case tag of
                     "Google" ->
-                        Jd.succeed Google
+                        Jd.succeed OpenIdConnectProviderGoogle
 
                     "GitHub" ->
-                        Jd.succeed GitHub
+                        Jd.succeed OpenIdConnectProviderGitHub
 
                     _ ->
                         Jd.fail ("OpenIdConnectProviderで不明なタグを受けたとった tag=" ++ tag)
@@ -389,13 +574,13 @@ languageJsonDecoder =
             (\tag ->
                 case tag of
                     "Japanese" ->
-                        Jd.succeed Japanese
+                        Jd.succeed LanguageJapanese
 
                     "English" ->
-                        Jd.succeed English
+                        Jd.succeed LanguageEnglish
 
                     "Esperanto" ->
-                        Jd.succeed Esperanto
+                        Jd.succeed LanguageEsperanto
 
                     _ ->
                         Jd.fail ("Languageで不明なタグを受けたとった tag=" ++ tag)
@@ -411,13 +596,13 @@ locationJsonDecoder =
             (\tag ->
                 case tag of
                     "Home" ->
-                        Jd.succeed Home
+                        Jd.succeed LocationHome
 
                     "User" ->
-                        Jd.field "userId" userIdJsonDecoder |> Jd.map User
+                        Jd.field "userId" userIdJsonDecoder |> Jd.map LocationUser
 
                     "Project" ->
-                        Jd.field "projectId" projectIdJsonDecoder |> Jd.map Project
+                        Jd.field "projectId" projectIdJsonDecoder |> Jd.map LocationProject
 
                     _ ->
                         Jd.fail ("Locationで不明なタグを受けたとった tag=" ++ tag)
@@ -446,3 +631,168 @@ userPublicJsonDecoder =
         |> Jdp.required "likedProjectIdList" (Jd.list projectIdJsonDecoder)
         |> Jdp.required "developedProjectIdList" (Jd.list projectIdJsonDecoder)
         |> Jdp.required "commentedIdeaIdList" (Jd.list ideaIdJsonDecoder)
+
+
+{-| UserPublicAndUserIdのJSON Decoder
+-}
+userPublicAndUserIdJsonDecoder : Jd.Decoder UserPublicAndUserId
+userPublicAndUserIdJsonDecoder =
+    Jd.succeed
+        (\userId userPublic ->
+            { userId = userId
+            , userPublic = userPublic
+            }
+        )
+        |> Jdp.required "userId" userIdJsonDecoder
+        |> Jdp.required "userPublic" userPublicJsonDecoder
+
+
+{-| ProjectのJSON Decoder
+-}
+projectJsonDecoder : Jd.Decoder Project
+projectJsonDecoder =
+    Jd.succeed
+        (\name icon image createdAt ->
+            { name = name
+            , icon = icon
+            , image = image
+            , createdAt = createdAt
+            }
+        )
+        |> Jdp.required "name" Jd.string
+        |> Jdp.required "icon" fileHashJsonDecoder
+        |> Jdp.required "image" fileHashJsonDecoder
+        |> Jdp.required "createdAt" dateTimeJsonDecoder
+
+
+{-| IdeaのJSON Decoder
+-}
+ideaJsonDecoder : Jd.Decoder Idea
+ideaJsonDecoder =
+    Jd.succeed
+        (\name createdAt commentList draftCommitIdList ->
+            { name = name
+            , createdAt = createdAt
+            , commentList = commentList
+            , draftCommitIdList = draftCommitIdList
+            }
+        )
+        |> Jdp.required "name" Jd.string
+        |> Jdp.required "createdAt" dateTimeJsonDecoder
+        |> Jdp.required "commentList" (Jd.list ideaCommentJsonDecoder)
+        |> Jdp.required "draftCommitIdList" (Jd.list suggestionJsonDecoder)
+
+
+{-| IdeaCommentのJSON Decoder
+-}
+ideaCommentJsonDecoder : Jd.Decoder IdeaComment
+ideaCommentJsonDecoder =
+    Jd.field "_" Jd.string
+        |> Jd.andThen
+            (\tag ->
+                case tag of
+                    "Text" ->
+                        Jd.field "ideaCommentText" ideaCommentTextJsonDecoder |> Jd.map IdeaCommentText
+
+                    "Suggestion" ->
+                        Jd.field "suggestion" suggestionJsonDecoder |> Jd.map IdeaCommentSuggestion
+
+                    _ ->
+                        Jd.fail ("IdeaCommentで不明なタグを受けたとった tag=" ++ tag)
+            )
+
+
+{-| IdeaCommentTextのJSON Decoder
+-}
+ideaCommentTextJsonDecoder : Jd.Decoder IdeaCommentText
+ideaCommentTextJsonDecoder =
+    Jd.succeed
+        (\body createdBy createdAt ->
+            { body = body
+            , createdBy = createdBy
+            , createdAt = createdAt
+            }
+        )
+        |> Jdp.required "body" Jd.string
+        |> Jdp.required "createdBy" userIdJsonDecoder
+        |> Jdp.required "createdAt" dateTimeJsonDecoder
+
+
+{-| SuggestionのJSON Decoder
+-}
+suggestionJsonDecoder : Jd.Decoder Suggestion
+suggestionJsonDecoder =
+    Jd.succeed
+        (\createdAt description change ->
+            { createdAt = createdAt
+            , description = description
+            , change = change
+            }
+        )
+        |> Jdp.required "createdAt" dateTimeJsonDecoder
+        |> Jdp.required "description" Jd.string
+        |> Jdp.required "change" changeJsonDecoder
+
+
+{-| ChangeのJSON Decoder
+-}
+changeJsonDecoder : Jd.Decoder Change
+changeJsonDecoder =
+    Jd.field "_" Jd.string
+        |> Jd.andThen
+            (\tag ->
+                case tag of
+                    "ProjectName" ->
+                        Jd.field "string_" Jd.string |> Jd.map ChangeProjectName
+
+                    _ ->
+                        Jd.fail ("Changeで不明なタグを受けたとった tag=" ++ tag)
+            )
+
+
+{-| ModuleSnapshotのJSON Decoder
+-}
+moduleSnapshotJsonDecoder : Jd.Decoder ModuleSnapshot
+moduleSnapshotJsonDecoder =
+    Jd.succeed
+        (\name description export ->
+            { name = name
+            , description = description
+            , export = export
+            }
+        )
+        |> Jdp.required "name" (Jd.list Jd.string)
+        |> Jdp.required "description" Jd.string
+        |> Jdp.required "export" Jd.bool
+
+
+{-| TypeSnapshotのJSON Decoder
+-}
+typeSnapshotJsonDecoder : Jd.Decoder TypeSnapshot
+typeSnapshotJsonDecoder =
+    Jd.succeed
+        (\name parentList description ->
+            { name = name
+            , parentList = parentList
+            , description = description
+            }
+        )
+        |> Jdp.required "name" Jd.string
+        |> Jdp.required "parentList" (Jd.list partIdJsonDecoder)
+        |> Jdp.required "description" Jd.string
+
+
+{-| PartSnapshotのJSON Decoder
+-}
+partSnapshotJsonDecoder : Jd.Decoder PartSnapshot
+partSnapshotJsonDecoder =
+    Jd.succeed
+        (\name parentList description ->
+            { name = name
+            , parentList = parentList
+            , description = description
+            }
+        )
+        |> Jdp.required "name" Jd.string
+        |> Jdp.required "parentList" (Jd.list partIdJsonDecoder)
+        |> Jdp.required "description" Jd.string
