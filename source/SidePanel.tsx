7ca3e905439d24cefd922ffaeecbf8f19fa4671d
narumincho <narumincho.starfy@gmail.com>
2020-06-12 14:44:13 +0900

    add GitHub icon

--- source/SidePanel.tsx
+++ source/SidePanel.tsx
@@ -18,12 +18,7 @@ export const SidePanel: React.FC<{
       height: "100%",
     }}
   >
-    <ui.Link
-      onJump={prop.onJump}
-      urlData={{ ...prop.urlData, location: data.Location.Home }}
-    >
-      <div css={{ color: "#b9d09b", fontSize: 32 }}>Definy</div>
-    </ui.Link>
+    <Logo onJump={prop.onJump} urlData={prop.urlData} />
     <LogInButton />
     <ui.Link
       onJump={prop.onJump}
@@ -36,9 +31,22 @@ export const SidePanel: React.FC<{
 
 SidePanel.displayName = "SidePanel";
 
+const Logo: React.FC<{
+  onJump: (urlData: data.UrlData) => void;
+  urlData: data.UrlData;
+}> = (prop) => (
+  <ui.Link
+    onJump={prop.onJump}
+    urlData={{ ...prop.urlData, location: data.Location.Home }}
+  >
+    <div css={{ color: "#b9d09b", fontSize: 32 }}>Definy</div>
+  </ui.Link>
+);
+
 const LogInButton = () => (
-  <div css={{ padding: 8 }}>
+  <div css={{ display: "grid", gap: 8, padding: 8 }}>
     <GoogleButton />
+    <GitHubButton />
   </div>
 );
 
@@ -84,6 +92,40 @@ const GoogleIcon = () => (
     <path
       d="M10 3.88c1.88 0 3.13.81 3.85 1.48l2.84-2.76C14.96.99 12.7 0 10 0 6.09 0 2.72 2.25 1.07 5.51l3.24 2.52C5.12 5.62 7.36 3.88 10 3.88z"
       fill="rgb(234, 67, 53)"
+    />
+  </svg>
+);
+
+const GitHubButton = () => (
+  <div
+    css={{
+      display: "grid",
+      gridTemplateColumns: "48px 1fr",
+      backgroundColor: "#202020",
+      borderRadius: 8,
+      gap: 8,
+    }}
+  >
+    <div
+      css={{
+        width: 48,
+        height: 48,
+        padding: 8,
+        backgroundColor: "#fff",
+        borderRadius: 8,
+      }}
+    >
+      <GitHubIcon />
+    </div>
+    <div css={{ alignSelf: "center", fontSize: 18 }}>Sign in with GitHub</div>
+  </div>
+);
+
+const GitHubIcon = () => (
+  <svg className="_7a20794b" viewBox="0 0 20 20">
+    <path
+      d="M10 0C4.476 0 0 4.477 0 10c0 4.418 2.865 8.166 6.84 9.49.5.09.68-.218.68-.483 0-.237-.007-.866-.012-1.7-2.782.603-3.37-1.34-3.37-1.34-.454-1.157-1.11-1.464-1.11-1.464-.907-.62.07-.608.07-.608 1.003.07 1.53 1.03 1.53 1.03.893 1.53 2.342 1.087 2.912.83.09-.645.35-1.085.634-1.335-2.22-.253-4.555-1.11-4.555-4.943 0-1.09.39-1.984 1.03-2.683-.105-.253-.448-1.27.096-2.647 0 0 .84-.268 2.75 1.026C8.294 4.95 9.15 4.84 10 4.836c.85.004 1.705.115 2.504.337 1.91-1.294 2.747-1.026 2.747-1.026.548 1.377.204 2.394.1 2.647.64.7 1.03 1.592 1.03 2.683 0 3.842-2.34 4.687-4.566 4.935.36.308.678.92.678 1.852 0 1.336-.01 2.415-.01 2.743 0 .267.18.578.687.48C17.14 18.163 20 14.417 20 10c0-5.522-4.478-10-10-10"
+      fill="rgb(0, 0, 0)"
     />
   </svg>
 );
