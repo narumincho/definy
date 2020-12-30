2bfa72f98e482f91b29f4703c0f79a3eb7628eea
narumincho <narumincho.starfy@gmail.com>
2020-12-30 17:11:07 +0900

    add oneLineTextInput

--- source/oneLineTextInput.ts
+++ source/oneLineTextInput.ts
@@ -1,6 +1,8 @@
 import { css, jsx as h } from "@emotion/react";
 import { ChangeEvent } from "react";
 import { Editor } from "./ui";
+import { Element } from "./view/view";
+import { inputOneLineText } from "./view/viewUtil";
 
 export const OneLineTextInput: Editor<string> = (props) => {
   if (typeof props.value !== "string") {
@@ -28,3 +30,25 @@ export const OneLineTextInput: Editor<string> = (props) => {
     }),
   });
 };
+
+export const oneLineTextEditor = <Message>(
+  value: string,
+  onInput: (text: string) => Message
+): Element<Message> =>
+  inputOneLineText({
+    value,
+    input: onInput,
+    style: {
+      padding: 8,
+      fontSize: 16,
+      border: "2px solid #222",
+      backgroundColor: "#000",
+      color: "#ddd",
+      borderRadius: 8,
+      width: "100%",
+      "&:focus": {
+        border: "2px solid #f0932b",
+        outline: "none",
+      },
+    },
+  });
