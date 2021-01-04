f017107dfb2a70db6fc7b22e394ba62b003d83b7
narumincho <narumincho.starfy@gmail.com>
2021-01-05 07:59:21 +0900

    use renderView

--- source/main.ts
+++ source/main.ts
@@ -1,24 +1,10 @@
 import * as app from "./app";
-import { createPatchState, domToView, patchView } from "./view/patch";
+import { createPatchState, patchView, renderView } from "./view/patch";
 import { View } from "./view/view";
 import { createViewDiff } from "./view/diff";
 
-const initView = domToView();
-if (initView._ === "Error") {
-  console.error("DOMの初期状態を解釈できなかった", initView.error);
-  throw new Error("DOMの初期状態を解釈できなかった");
-}
-
 const pushMessageList = (message: app.Message): void => {
   messageList.push(message);
-};
-
-const render = () => {
-  const newView = app.stateToView(state);
-  const diff = createViewDiff(oldView, newView);
-  console.log("view diff", oldView, newView, diff);
-  oldView = newView;
-  patchView(diff, patchState);
 };
 
 const loop = () => {
@@ -34,14 +20,16 @@ const loop = () => {
     }
     state = app.updateStateByMessage(pushMessageList, message, state);
   }
-  render();
+  const newView = app.stateToView(state);
+  const diff = createViewDiff(oldView, newView);
+  console.log("view diff", oldView, newView, diff);
+  oldView = newView;
+  patchView(diff, patchState);
 };
 
 const messageList: Array<app.Message> = [];
-let oldView: View<app.Message> = initView.ok;
 let state: app.State = app.initState(pushMessageList);
+let oldView: View<app.Message> = app.stateToView(state);
 const patchState = createPatchState(pushMessageList);
-if (messageList.length === 0) {
-  render();
-}
+renderView(oldView, patchState);
 loop();
