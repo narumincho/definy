fe860dd46c5ff2d42dc0e577d335c9bc92e6be05
narumincho <narumincho.starfy@gmail.com>
2021-03-20 18:55:42 +0900

    chore: jest の env を TypeScript で

--- test/env.ts
+++ test/env.ts
@@ -1,3 +1,13 @@
+/**
+ * jest の 使用する js-dom で グローバルに定義された
+ *
+ * - TextEncoder
+ * - TextDecoder
+ *
+ * がサポートされていないため. この環境設定をする
+ */
+/* eslint-disable @typescript-eslint/no-require-imports */
+/* eslint-disable @typescript-eslint/no-var-requires */
 const Environment = require("jest-environment-jsdom");
 
 module.exports = class CustomTestEnvironment extends Environment {
