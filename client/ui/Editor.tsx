a66d8e5544907124a4f6b4647d027d2be7c8c68e
narumincho <narumincho.starfy@gmail.com>
2021-04-01 13:24:27 +0900

    feat(react): Editor の追加

---
+++ client/ui/Editor.tsx
@@ -0,0 +1,169 @@
+import * as React from "react";
+import { css } from "@emotion/css";
+
+export type TypeAndValue =
+  | {
+      type: "text";
+      value: string;
+    }
+  | {
+      type: "number";
+      value: number;
+    }
+  | {
+      type: "select";
+      valueList: ReadonlyArray<string>;
+      index: number;
+    };
+
+export type Item = {
+  name: string;
+  typeAndValue: TypeAndValue;
+};
+
+export type Props = {
+  readonly headItem: { typeAndValue: TypeAndValue };
+  readonly items: ReadonlyArray<Item>;
+};
+
+type Selection =
+  | {
+      tag: "none";
+    }
+  | {
+      tag: "head";
+    }
+  | {
+      tag: "content";
+      index: number;
+    };
+
+/**
+ * 要素の操作対象を選ぶ, SelectionView, 選択した対象を操作する DetailView を内包する
+ */
+export const Editor: React.VFC<Props> = (props) => {
+  const [selection, setSelection] = React.useState<Selection>({
+    tag: "none",
+  });
+  return (
+    <div
+      className={css({
+        display: "grid",
+        gridTemplateColumns: "1fr 300px",
+      })}
+    >
+      <div>
+        <div
+          className={css({
+            display: "flex",
+            borderWidth: 2,
+            borderStyle: "solid",
+            borderColor: selection.tag === "head" ? "red" : "transparent",
+          })}
+          onClick={() => {
+            setSelection({ tag: "head" });
+          }}
+        >
+          <ValueView typeAndValue={props.headItem.typeAndValue} isBig />
+        </div>
+        {props.items.map((item, index) => (
+          <div
+            key={item.name}
+            className={css({
+              padding: 8,
+              borderWidth: 2,
+              borderStyle: "solid",
+              borderColor:
+                selection.tag === "content" && selection.index === index
+                  ? "red"
+                  : "transparent",
+            })}
+            onClick={() => {
+              setSelection({ tag: "content", index });
+            }}
+          >
+            <div
+              className={css({
+                display: "flex",
+                gap: 16,
+                alignItems: "center",
+              })}
+            >
+              <div
+                className={css({
+                  fontSize: 24,
+                })}
+              >
+                {item.name}
+              </div>
+              <TypeView typeAndValue={item.typeAndValue} />
+            </div>
+            <ValueView typeAndValue={item.typeAndValue} />
+          </div>
+        ))}
+      </div>
+      <div>DetailView</div>
+    </div>
+  );
+};
+
+const TypeView: React.VFC<{ typeAndValue: TypeAndValue }> = (props) => {
+  switch (props.typeAndValue.type) {
+    case "number":
+      return (
+        <div
+          className={css({
+            color: "#ddd",
+          })}
+        >
+          number
+        </div>
+      );
+    case "text":
+      return (
+        <div
+          className={css({
+            color: "#ddd",
+          })}
+        >
+          text
+        </div>
+      );
+    case "select":
+      return (
+        <div
+          className={css({
+            color: "#ddd",
+          })}
+        >
+          option(
+          {props.typeAndValue.valueList.join(",")})
+        </div>
+      );
+  }
+};
+
+const ValueView: React.VFC<{ typeAndValue: TypeAndValue; isBig?: boolean }> = (
+  props
+) => {
+  switch (props.typeAndValue.type) {
+    case "number":
+      return (
+        <div className={css({ fontSize: props.isBig ? 32 : 16 })}>
+          {props.typeAndValue.value}
+        </div>
+      );
+    case "text":
+      return (
+        <div className={css({ fontSize: props.isBig ? 32 : 16 })}>
+          {props.typeAndValue.value}
+        </div>
+      );
+    case "select":
+      return (
+        <div className={css({ fontSize: props.isBig ? 32 : 16 })}>
+          {props.typeAndValue.valueList[props.typeAndValue.index]}
+        </div>
+      );
+  }
+};
---
+++ stories/Editor.tsx
@@ -0,0 +1,69 @@
+import * as React from "react";
+import { Editor, Props } from "../client/ui/Editor";
+import { Meta, Story } from "@storybook/react";
+import { ArgType } from "@storybook/addons";
+
+const argTypes: Record<string, ArgType> = {};
+
+const meta: Meta = {
+  title: "Editor",
+  component: Editor,
+  argTypes,
+};
+export default meta;
+
+export const Project: Story<never> = () => (
+  <Editor
+    headItem={{ typeAndValue: { type: "text", value: "やあ" } }}
+    items={[
+      {
+        name: "作成者",
+        typeAndValue: { type: "text", value: "作成者の名前" },
+      },
+      {
+        name: "作成日時",
+        typeAndValue: { type: "text", value: "2021-04-01" },
+      },
+      {
+        name: "パーツ",
+        typeAndValue: { type: "text", value: "パーツのリストを表示したい" },
+      },
+      {
+        name: "型パーツ",
+        typeAndValue: { type: "text", value: "型パーツのリストを表示したい" },
+      },
+    ]}
+  />
+);
+
+export const TypePart: Story<never> = () => (
+  <Editor
+    headItem={{ typeAndValue: { type: "text", value: "Location" } }}
+    items={[
+      {
+        name: "description",
+        typeAndValue: {
+          type: "text",
+          value:
+            "DefinyWebアプリ内での場所を示すもの. URLから求められる. URLに変換できる",
+        },
+      },
+      {
+        name: "attribute",
+        typeAndValue: {
+          type: "select",
+          valueList: ["Just", "Nothing"],
+          index: 1,
+        },
+      },
+      {
+        name: "body",
+        typeAndValue: {
+          type: "select",
+          valueList: ["Product", "Sum", "Kernel"],
+          index: 1,
+        },
+      },
+    ]}
+  />
+);
