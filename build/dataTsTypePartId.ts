9b51b16361302aebc6820a16ce907a0a0815a0be
narumincho <narumincho.starfy@gmail.com>
2021-07-19 11:52:14 +0900

    wip

---
+++ build/dataTsTypePartId.ts
@@ -0,0 +1,150 @@
+import * as d from "../data";
+
+const from = d.TypePartId.fromString;
+
+export const elmVariant = from("0bc2e5ab2fd2caeb6e6c331573ae735f");
+export const exportDefinition = from("0c8b854b812290a054877ca5de6c4daf");
+export const unaryOperatorExpr = from("0cb0a34b3953e57b30f4a5c8f9d305da");
+export const projectId = from("0cd913118a2ad1469d656146a08c5f76");
+export const language = from("0d9510723f4d34605e41f3a47a8b0ea7");
+export const elmTypeName = from("13e52324ba81c7ffe655455ad739ea73");
+export const int32 = from("16c2d8602e3e5e2d38b9215bd0c44d8d");
+export const elmType = from("173f661790b059baf212043f816ba1a9");
+export const tsType = from("1835ebdc909b0fa93d11ae0ed93d0606");
+export const resourceState = from("18cbd2fa4bd72a0eacf3f0f44aeb6feb");
+export const unit = from("217263d8cdca613c2d1ba77e19d4cbb0");
+export const typeAttribute = from("22d8f2d392c573a7aa54c39fa4c98ad0");
+export const elmField = from("2392b884a9c2238a65ecd272bd23a9cd");
+export const tsTypeWithTypeParameter = from("23fa0e8b66c9d093d4c18579ffefaaa3");
+export const evaluateExprError = from("25d0af5522da614f7617016ca7868edd");
+export const tsIdentifer = from("2a75ad7c7ccc58ba56f6c8545c8150d1");
+export const elmLocalType = from("2b42264a878355f0257141fa013fed5c");
+export const functionCall = from("2f9784d2c41d0974487a1ca2b0a51ec5");
+export const maybe = from("304f21ae8208a21d08731aa6d183742d");
+export const arrayItem = from("35f7e442c0838f9982e6b49da398d97f");
+export const getClassAndRoleParameter = from(
+  "37e7ea136171a7fd4e23be7a7385af6b"
+);
+export const getQuestionListInProgramByQuestionIdParameter = from(
+  "38f8436dbd5f16407883acb3917cca49"
+);
+export const binary = from("3e2f740c88923b0393a1ef93d92f157b");
+export const elmTuple3 = from("42e5ab4936af4abf8673472ce96fd794");
+export const elmImportedType = from("43aa21ed2225cacc4489c30113e87787");
+export const binaryOperator = from("4953938f040aefdaa5f2601e2a78d43b");
+export const typeAssertion = from("49f05a756bd75da670fa5b64ed8bf1ae");
+export const unaryOperator = from("4b4c3f8f34d1c05bb9d7d1b930bc9c9a");
+export const typePartBody = from("4c60c73d6b8f236d34281fbb9119a598");
+export const qCreateClassParameter = from("4d9e178c23a42a4dc2e2b68270d50363");
+export const elmCustomTypeExportLevel = from(
+  "4e1cab8c9074c1a058d6ef3ffcf29ab0"
+);
+export const qAccount = from("4e6857a774597ae66e5c316642a8ae8b");
+export const qProgramId = from("4fe2f5da0a887c2a6b33c16c5d52058f");
+export const getClassParticipantParameter = from(
+  "510d8ea5d62965942071b7ed6b4edae8"
+);
+export const qQuestionId = from("5249c5510c734bbe48e63a23e4e202b3");
+export const parameter = from("5433bade7738da21e7663ff043f588d5");
+export const dict = from("5516d41d180b98b7f5a8950d4f43dfd6");
+export const withTime = from("55d4de38cfb884b9008abd7f7f63325e");
+export const elmTypeAlias = from("57f429fd5c67eb2542447a2eaeb34497");
+export const partId = from("5880cd7c0b6f0205c739a06181898d03");
+export const accountTokenAndUserId = from("5d291325e9d7bc27ff35e907f59bde54");
+export const tagId = from("5ec292c48424e5e9ee8cc2d672830ecd");
+export const elmFunctionType = from("5ec9dae951d8d2b250c81185a259adca");
+export const tsPattern = from("6126431def47ecba239b3add7f472b19");
+export const qClassId = from("624e37b7230f763e6318c627c2c728ec");
+export const dataTypeParameter = from("627dc8fa15214481812af12268d97b6b");
+export const elmCode = from("62923d32a2730114e665cef13f93ff13");
+export const typePartBodyKernel = from("657b5a5519b095480a21fdadd0f6fbd1");
+export const string = from("666361c9ae68ab64f273c47f3feb8c73");
+export const account = from("67d4e3bc4f13e1132b6c5e81d5b74395");
+export const staticResourceState = from("68d925f7b35bab8f265cd9e5eb7ea3da");
+export const setStatement = from("6a5a7886b0f117a50d9d7b11ffe87c9f");
+export const parameterWithDocument = from("6b05e33009ade6b4decd452e8fd4bff1");
+export const typePartId = from("6e3cff317f8bfbbd1391c0afb9ad6b72");
+export const result = from("6f937e46fce1cf70d29c54780f132c18");
+export const urlData = from("7210f04a85c5f0f58f7aa20826d67f05");
+export const switchStatement = from("725f84ef82e6c3b5b5f6fd5208977dd8");
+export const variableDefinitionStatement = from(
+  "7277cf2483388ee2f5ad7a7fc8585d98"
+);
+export const dataTypeOrDataTypeParameter = from(
+  "7b01edcb8f7b313ab03aa6b376876545"
+);
+export const createProjectParameter = from("7b987551cc112a64f5c9a8cb1647f4bd");
+export const statement = from("7c957d411f448a03764f16d43e6e5008");
+export const time = from("7d91f0f70643799692aa144ee51a62b1");
+export const accountTokenAndProjectId = from(
+  "7e4613f18e8e3bcfdfc00356654414d5"
+);
+export const dataType = from("7e689d3cd011d42947cca759ecebd1cf");
+export const importedVariable = from("8150e2833dc1f2a77715aa1bb7b2baad");
+export const openIdConnectProvider = from("84597034eb252267ce1a599ab7a0b543");
+export const typePart = from("87fc411d10f6986dd33e5dbaff6d06ae");
+export const qNewQuestionParameter = from("8960809c7ca4954e9235c197f5d63cdf");
+export const projectDataFile = from("8aa41233812aab908b1d71d11ad97375");
+export const tsExpr = from("8b0bdbeae05df3badac2ee69bf22011f");
+export const elmTuple2 = from("8b479d8726bb494ac76d529fbf03908c");
+export const project = from("8bc81fa2a15bdbd8c2414d22f3e1b462");
+export const forOfStatement = from("8be8db4404f302dd36c8a2c6c6e37e7c");
+export const jsTsCode = from("92ea504049ca5b7e2227a95195fe74eb");
+export const member = from("9481ceae2c3e2765243f5c7ce7eb76c7");
+export const qAccountData = from("94aa0a477dfb8362d00aaac05b29b79c");
+export const elmExpr = from("94e53139b3a82087777ea3e001b2adb4");
+export const functionType = from("98904215e50e04fa2197a7a96df8f646");
+export const qEditQuestion = from("98bc46a897ffdb9d86782890776c9221");
+export const tuple2 = from("9b55e2a5ed07246355fbeba998eaf5fe");
+export const elmCustomType = from("a0cee28e982f5fbcae6b2c7bd1e27e2a");
+export const callExpr = from("a1f71188b7c3ab2a21954471f3ac67f1");
+export const variable = from("a2580a29c047f54d635c4456696bbef3");
+export const qClassInvitationToken = from("a5f14e3ce94846347dc38bc1fb29ba7f");
+export const qCreateProgramParameter = from("a86e36ceeff2a4fefb8d9147c232dbb6");
+export const kernelCall = from("a8c830bd6b3473f2fac7a62ede58ecf0");
+export const getExpr = from("a9ce526a22f09800aa3773a1614cce89");
+export const qCreateQuestionParamter = from("aa65c956d46bd985c325c3567ad65abb");
+export const qQuestion = from("ab9150e30802a0e39f7fdca3703da3ab");
+export const type = from("b3b36f39469d23321ed01b92f048ccc0");
+export const importedType = from("b44289bc65640d4ad0ad79a5a627ef78");
+export const accountTokenHash = from("b553ab17ca45f4975d9fe17fe1a63ac4");
+export const kernelExpr = from("b6eef263a982482747a8ad0bc9f05e21");
+export const projectDataFileHashValue = from(
+  "b8bf6c22e275c06c7dc42300c332b47d"
+);
+export const location = from("bbcb8e43df8afff9fe24b001c66fb065");
+export const typeAlias = from("bf69f7e46f5e4949fe801c234fc3ad15");
+export const accountId = from("c1a17b07e8e664ca80217833ff2ad3f1");
+export const keyValue = from("c2428cbcd62b33ce2c904a200b876863");
+export const list = from("c3fc2a6cea61086db59e11dc2bef0eee");
+export const logInState = from("c4b574e3ca8bad17022054d5e77fd3d0");
+export const forStatement = from("c53be8bcaf56ff90080cf7a75f1e4815");
+export const tsMemberType = from("c68bac5afb1a1d35af096bf317d09339");
+export const conditionalOperatorExpr = from("c7a771f770f0f92690faeb59eaff6268");
+export const ifStatement = from("cd325760f992e47a8438ae78ca9b1529");
+export const elmFieldName = from("d256109676f834b74931495891b61d1b");
+export const elmVariantName = from("d77f1f967b892c739a1555f74d2ff9fd");
+export const intersectionType = from("d82150857d0f2e78c1df0f418ba9b01a");
+export const part = from("d8bccc70252cee9ce70784bf5dfa493b");
+export const evaluatedExpr = from("daa9bf22c3f2e52ff92edcdd896d6a6d");
+export const setTypePartParameter = from("dac038758a5c45f762de388bb5193fb8");
+export const bool = from("db57caf129f60db969b5b5224a72b937");
+export const elmDefinition = from("db8b9f83d99ef58fd206acca4e56d098");
+export const function_ = from("dd3366ce7254327c85732d2932ff219b");
+export const accountToken = from("deb01f82879754c03aa4d244e136e59d");
+export const functionDefinitionStatement = from(
+  "df5c5388248252a92864ceb219628b8e"
+);
+export const pattern = from("e4ef8a0713aceb0eaa7606cd516ad847");
+export const imageHash = from("e71e15a15b0883940a75e58367151d1a");
+export const expr = from("ed9a046b5f121f6e54ac7f3834cbc644");
+export const tagReference = from("edd59bd2ab4852c149f6ce5917767908");
+export const mode = from("ee0590e764618611ffa8e1a0a2e22f79");
+export const binaryOperatorExpr = from("eec2dab74fbcd3237bfa1c773f9b9d21");
+export const codeType = from("eedf73c97f9dd61159572322f6d7e0a5");
+export const lambdaExpr = from("eeeef818144e5a42460c4efdaa221460");
+export const requestLogInUrlRequestData = from(
+  "f03ea5331c1a3adcde80a04054d35e07"
+);
+export const elmTypeDeclaration = from("f4bed793b657b39db5a579c9e158eba5");
+export const tsMember = from("fddd2a65994fae205dd636f3a6b9f1ea");
