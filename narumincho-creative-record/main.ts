4e610bb6a613cdc301ba4a8f2b47bf7d1d8fad9a
narumincho <narumincho.starfy@gmail.com>
2021-08-08 14:36:45 +0900

    [creative-record] アイコンの表示

--- narumincho-creative-record/main.ts
+++ narumincho-creative-record/main.ts
@@ -1,29 +1,47 @@
 import { fastify } from "fastify";
+import { promises as fileSystem } from "fs";
 import { html } from "../gen/main";
 
-const instance = fastify();
+const portNumber = 8080;
+const origin = `http://localhost:${portNumber}`;
+const iconPath = "/icon";
 
+const instance = fastify();
 instance.get("/", (request, reply) => {
   reply.type("text/html");
   reply.send(
     html.htmlOptionToString({
       appName: "ナルミンチョの創作記録",
       children: [html.div({}, "やあ")],
-      coverImageUrl: new URL("https://narumincho.com/icon"),
+      coverImageUrl: new URL(origin + iconPath),
       description:
         "革新的なプログラミング言語のDefiny, Web技術, 作っているゲームなどについて解説しています",
-      iconUrl: new URL("https://narumincho.com/icon"),
+      iconUrl: new URL(origin + iconPath),
       language: "Japanese",
       pageName: "ナルミンチョの創作記録",
       scriptUrlList: [],
       styleUrlList: [],
       twitterCard: "SummaryCard",
-      url: new URL("http://localhost:8080"),
+      url: new URL(origin),
       themeColor: undefined,
     })
   );
 });
+instance.get(iconPath, (request, reply): void => {
+  console.log("アイコンをリクエストされた");
+  reply.type("image/png");
+  fileSystem
+    .readFile("./narumincho-creative-record/icon.png")
+    .then((iconImageFile) => {
+      console.log(iconImageFile);
+      reply.send(iconImageFile);
+    })
+    .catch((err) => {
+      console.log("アイコンファイル読み込み時にエラー", err);
+    });
+});
 
-instance.listen(8080);
-console.log("http://localhost:8080");
+instance.listen(portNumber);
+console.log(`ナルミンチョ創作記録開発サーバー起動中! → ${origin}`);
+
 export {};
