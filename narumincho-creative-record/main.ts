e2b023e76d6289a68d238765cbce3dce4eb5f231
narumincho <narumincho.starfy@gmail.com>
2021-08-08 14:14:41 +0900

    [creative-record] gen/html を使ってHTMLを生成

--- narumincho-creative-record/main.ts
+++ narumincho-creative-record/main.ts
@@ -1,24 +1,27 @@
 import { fastify } from "fastify";
+import { html } from "../gen/main";
 
 const instance = fastify();
 
 instance.get("/", (request, reply) => {
   reply.type("text/html");
-  reply.send(`<!DOCTYPE html>
-  <html lang="ja">
-  
-  <head>
-      <meta charset="UTF-8">
-      <meta name="viewport" content="width=device-width, initial-scale=1.0">
-      <title>ナルミンチョの創作記録 リニューアルテスト</title>
-      </script>
-  </head>
-  
-  <body>
-    ナルミンチョの創作記録 リニューアルテスト
-  </body>
-  
-  </html>`);
+  reply.send(
+    html.htmlOptionToString({
+      appName: "ナルミンチョの創作記録",
+      children: [html.div({}, "やあ")],
+      coverImageUrl: new URL("https://narumincho.com/icon"),
+      description:
+        "革新的なプログラミング言語のDefiny, Web技術, 作っているゲームなどについて解説しています",
+      iconUrl: new URL("https://narumincho.com/icon"),
+      language: "Japanese",
+      pageName: "ナルミンチョの創作記録",
+      scriptUrlList: [],
+      styleUrlList: [],
+      twitterCard: "SummaryCard",
+      url: new URL("http://localhost:8080"),
+      themeColor: undefined,
+    })
+  );
 });
 
 instance.listen(8080);
