ac94da2fe43d219b2722dde990ffb3205a667921
narumincho <narumincho.starfy@gmail.com>
2021-08-08 14:08:32 +0900

    [creative-record] 開発用サーバーを用意

--- narumincho-creative-record/main.ts
+++ narumincho-creative-record/main.ts
@@ -1,3 +1,26 @@
-console.log("ナルミンチョの創作記録をここで作ろうかな");
+import { fastify } from "fastify";
 
+const instance = fastify();
+
+instance.get("/", (request, reply) => {
+  reply.type("text/html");
+  reply.send(`<!DOCTYPE html>
+  <html lang="ja">
+  
+  <head>
+      <meta charset="UTF-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <title>ナルミンチョの創作記録 リニューアルテスト</title>
+      </script>
+  </head>
+  
+  <body>
+    ナルミンチョの創作記録 リニューアルテスト
+  </body>
+  
+  </html>`);
+});
+
+instance.listen(8080);
+console.log("http://localhost:8080");
 export {};
