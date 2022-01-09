e758305fc96b6b874cbec20fc33621835412abe1
narumincho <narumincho.starfy@gmail.com>
2022-01-09 21:34:20 +0900

    fix output/TypeScriptEntryPoint/index.d.ts

--- output/TypeScriptEntryPoint/index.d.ts
+++ output/TypeScriptEntryPoint/index.d.ts
@@ -40,71 +40,8 @@ export type NonEmptyString = string & { _nonEmpty: never };
 
 export type TwitterCard = { _twitterCard: never };
 
-/* eslint-disable init-declarations */
-export type HtmlOption = {
-  /**
-   * ページ名
-   * Google 検索のページ名や, タブ, ブックマークのタイトル, OGPのタイトルなどに使用される
-   */
-  readonly pageName: NonEmptyString;
-
-  /**
-   * アプリ名 / サイト名 (HTML出力のみ反映)
-   */
-  readonly appName: NonEmptyString;
-
-  /**
-   * ページの説明 (HTML出力のみ反映)
-   */
-  readonly description: string;
-
-  /**
-   * テーマカラー
-   */
-  readonly themeColor: Color.Color;
-
-  /**
-   * アイコン画像のURL
-   */
-  readonly iconPath: StructuredUrl.PathAndSearchParams;
-
-  /**
-   * 使用している言語
-   */
-  readonly language: Maybe.Maybe<Language.Language>;
-
-  /**
-   * OGPに使われるカバー画像のURL (CORSの制限を受けない)
-   */
-  readonly coverImagePath: StructuredUrl.PathAndSearchParams;
-
-  /**
-   * オリジン
-   */
-  readonly origin: NonEmptyString;
-
-  /**
-   * パス. ログイン時のコールバック時には Noting にして良い
-   */
-  readonly path: Maybe.Maybe<StructuredUrl.PathAndSearchParams>;
-
-  /**
-   * コンテンツ作成者のTwitterID `@`を含む
-   */
-  readonly creatorTwitterId: Maybe.Maybe<string>;
-
-  /**
-   * 全体に適応されるスタイル. CSS
-   */
-  readonly style: Maybe.Maybe<string>;
-  /** *スタイルのパス */
-  readonly stylePath: Maybe.Maybe<StructuredUrl.PathAndSearchParams>;
-  /** スクリプトのパス */
-  readonly scriptPath: Maybe.Maybe<StructuredUrl.PathAndSearchParams>;
-  /** body の class */
-  readonly bodyClass: Maybe.Maybe<NonEmptyString.NonEmptyString>;
-  /** body の 子要素 */
-  readonly bodyChildren: Array<never>;
+export type StructuredUrl = {
+  _structuredUrl: never;
 };
 
 export type PathAndSearchParams = {
