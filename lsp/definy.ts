000658a93e01fa5dbb6596c060b8f6c8a44aa1f6
narumincho <narumincho.starfy@gmail.com>
2022-02-09 21:49:04 +0900

    エラーメッセージの表示

--- lsp/definy.ts
+++ lsp/definy.ts
@@ -1,4 +1,5 @@
 export {};
+import { Diagnostic } from "vscode-languageclient/node";
 
 const main = (args: ReadonlyArray<string>): void => {
   if (args.length !== 3) {
@@ -13,50 +14,77 @@ const main = (args: ReadonlyArray<string>): void => {
 };
 
 const runLanguageServer = (): void => {
-  receiveJsonRpcMessage((request) => {
-    if (request.type === "error") {
+  receiveJsonRpcMessage(handleRequest);
+};
+
+const handleRequest = (request: JsonRpcRequestOrParseError): void => {
+  if (request.type === "error") {
+    sendJsonRpcMessage({
+      method: "window/logMessage",
+      params: {
+        type: 3,
+        message: `理解できないメッセージが来ました message:${
+          request.message
+        }, rawObject:${JSON.stringify(request.rawObject)}
+              `,
+      },
+    });
+    return;
+  }
+  switch (request.method) {
+    case "initialize": {
       sendJsonRpcMessage({
         method: "window/logMessage",
         params: {
           type: 3,
-          message: `理解できないメッセージが来ました message:${
-            request.message
-          }, rawObject:${JSON.stringify(request.rawObject)}
-          `,
+          message: "initialize 来たぜ",
+        },
+      });
+      sendJsonRpcMessage({
+        id: request.id,
+        result: {
+          capabilities: {
+            textDocumentSync: 1,
+          },
         },
       });
       return;
     }
-    switch (request.method) {
-      case "initialize": {
-        sendJsonRpcMessage({
-          method: "window/logMessage",
-          params: {
-            type: 3,
-            message: "initialize 来たぜ",
-          },
-        });
-        sendJsonRpcMessage({
-          id: request.id,
-          result: {
-            capabilities: {
-              textDocumentSync: 1,
-            },
-          },
-        });
-        return;
-      }
-      case "initialized": {
-        sendJsonRpcMessage({
-          method: "window/logMessage",
-          params: {
-            type: 3,
-            message: "initialized 来たぜ",
-          },
-        });
-      }
+    case "initialized": {
+      sendJsonRpcMessage({
+        method: "window/logMessage",
+        params: {
+          type: 3,
+          message: "initialized 来たぜ",
+        },
+      });
+      return;
     }
-  });
+    case "textDocument/didOpen": {
+      sendJsonRpcMessage({
+        method: "textDocument/publishDiagnostics",
+        params: {
+          uri: request.params.textDocument.uri,
+          diagnostics: [
+            {
+              range: {
+                start: { line: 0, character: 0 },
+                end: { line: 0, character: 5 },
+              },
+              message: "エラーメッセージ1",
+            },
+            {
+              range: {
+                start: { line: 1, character: 0 },
+                end: { line: 1, character: 5 },
+              },
+              message: "エラーメッセージ2",
+            },
+          ],
+        },
+      });
+    }
+  }
 };
 
 type JsonRpcRequestOrParseError =
@@ -69,6 +97,11 @@ type JsonRpcRequestOrParseError =
       readonly type: "ok";
       readonly id: number;
       readonly method: "initialize" | "initialized";
+    }
+  | {
+      readonly type: "ok";
+      readonly method: "textDocument/didOpen" | "textDocument/didChange";
+      readonly params: { readonly textDocument: { readonly uri: string } };
     };
 
 /**
@@ -126,22 +159,59 @@ const stringToJsonRpcMessage = (
 type JsonRpcRequestLooseObject = {
   readonly id: unknown;
   readonly method: unknown;
+  readonly params: unknown;
 };
 
 const objectToJsonRpcMessage = (
   message: JsonRpcRequestLooseObject
 ): JsonRpcRequestOrParseError | "need more" => {
-  if (typeof message.id !== "number") {
-    return { type: "error", message: "unknown id type", rawObject: message };
-  }
-  if (message.method === "initialize" || message.method === "initialized") {
+  if (
+    typeof message.id === "number" &&
+    (message.method === "initialize" || message.method === "initialized")
+  ) {
     return {
       type: "ok",
       id: message.id,
       method: message.method,
     };
   }
+
+  if (
+    message.method === "textDocument/didOpen" ||
+    message.method === "textDocument/didChange"
+  ) {
+    const params = getTextDocumentParams(message.params);
+    if (params === undefined) {
+      return { type: "error", message: "unknown param", rawObject: message };
+    }
+    return {
+      type: "ok",
+      method: message.method,
+      params,
+    };
+  }
   return { type: "error", message: "unknown method...", rawObject: message };
+};
+
+const getTextDocumentParams = (
+  value: unknown
+): { readonly textDocument: { readonly uri: string } } | undefined => {
+  if (value === null || typeof value !== "object") {
+    return undefined;
+  }
+  const textDocument = (value as { readonly textDocument: unknown })
+    .textDocument;
+  if (textDocument === null || typeof textDocument !== "object") {
+    return undefined;
+  }
+  const uri = (textDocument as { readonly uri: unknown }).uri;
+  if (typeof uri === "string") {
+    return {
+      textDocument: {
+        uri,
+      },
+    };
+  }
 };
 
 type JsonRpcResponse =
@@ -156,6 +226,13 @@ type JsonRpcResponse =
       readonly id: number;
       readonly result: {
         readonly capabilities: { readonly textDocumentSync: 1 };
+      };
+    }
+  | {
+      readonly method: "textDocument/publishDiagnostics";
+      readonly params: {
+        readonly uri: string;
+        readonly diagnostics: ReadonlyArray<Diagnostic>;
       };
     };
 
