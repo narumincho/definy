381494d295f46a2e79058bc8e45e262c09293b34
narumincho <16481886+narumincho@users.noreply.github.com>
2022-08-11 08:35:48 +0900

    nextjs に移行?

---
+++ next/pages/index.tsx
@@ -0,0 +1,12 @@
+import * as React from "react";
+import { css } from "@emotion/css";
+
+const TopPage = () => {
+  return (
+    <div className={css({ color: "white", backgroundColor: "green" })}>
+      トップページだよ!
+    </div>
+  );
+};
+
+export default TopPage;
