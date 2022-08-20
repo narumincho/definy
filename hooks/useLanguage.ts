39bcf85f84be143d290295bb5561bb5da46cde6b
narumincho <16481886+narumincho@users.noreply.github.com>
2022-08-20 09:25:36 +0900

    index.ts にリンクを追加

---
+++ hooks/useLanguage.ts
@@ -0,0 +1,29 @@
+import {
+  dataLanguageToQueryValue,
+  defaultLanguage,
+  languageQueryKey,
+  queryValueToDataLanguage,
+} from "../common/url";
+import type { Language } from "../localData";
+import { useQueryBasedState } from "./useQueryBasedState";
+
+/**
+ * Next.js の `useRouter` から言語のパラメータ(`hl`)を受け取り, 余計なパラメータを削除する
+ * @returns
+ */
+export const useLanguage = (): Language => {
+  const state = useQueryBasedState({
+    queryToStructuredQuery: (q) => queryValueToLanguage(q[languageQueryKey]),
+    structuredQueryToQuery: (s) => ({
+      [languageQueryKey]: dataLanguageToQueryValue(s),
+    }),
+  });
+  return state ?? defaultLanguage;
+};
+
+const queryValueToLanguage = (queryValue: unknown): Language => {
+  if (typeof queryValue === "string") {
+    return queryValueToDataLanguage(queryValue);
+  }
+  return defaultLanguage;
+};
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,14 @@
 import * as React from "react";
+import * as d from "../localData";
 import Head from "next/head";
 import { Header } from "../components/Header";
+import { Link } from "../components/Link";
 import iconPng from "../assets/icon.png";
+import { useLanguage } from "../hooks/useLanguage";
 
 const IndexPage = (): React.ReactElement => {
+  const language = useLanguage();
+
   return (
     <>
       <Head>
@@ -13,10 +18,10 @@ const IndexPage = (): React.ReactElement => {
       <div
         css={{
           display: "grid",
-          placeContent: "center",
           height: "100%",
           color: "white",
           backgroundColor: "black",
+          gridTemplateRows: "auto 1fr",
         }}
         lang="ja"
       >
@@ -40,10 +45,60 @@ const IndexPage = (): React.ReactElement => {
             language: "Japanese",
           }}
         />
-        definy 整備中...
+        <div>
+          <div css={{ padding: 16 }}>
+            <HomeLinkList language={language} />
+          </div>
+          definy 整備中...
+        </div>
       </div>
     </>
   );
 };
 
 export default IndexPage;
+
+const HomeLinkList = (props: {
+  readonly language: d.Language;
+}): React.ReactElement => {
+  return (
+    <div
+      css={{
+        display: "grid",
+        gridAutoFlow: "column",
+        justifyContent: "end",
+        alignItems: "center",
+        height: 32,
+        gap: 8,
+      }}
+    >
+      <Link
+        locationAndLanguage={{
+          location: d.Location.About,
+          language: props.language,
+        }}
+        style={{ padding: 4 }}
+      >
+        definyについて
+      </Link>
+      <Link
+        locationAndLanguage={{
+          location: d.Location.LocalProject,
+          language: props.language,
+        }}
+        style={{ padding: 4 }}
+      >
+        ファイルから開く
+      </Link>
+      <Link
+        locationAndLanguage={{
+          location: d.Location.ToolList,
+          language: props.language,
+        }}
+        style={{ padding: 4 }}
+      >
+        ツール
+      </Link>
+    </div>
+  );
+};
