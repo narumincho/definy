e1a2bf011be615528d3e8e9e6fc59741e47f0608
narumincho <16481886+narumincho@users.noreply.github.com>
2022-08-29 12:01:42 +0900

    環境変数が存在するかチェックするように

--- functions/environmentVariables.ts
+++ functions/environmentVariables.ts
@@ -1,9 +1,23 @@
-export const FAUNA_SERVER_KEY = process.env.FAUNA_SERVER_KEY as string;
+const expectString = (message: string, value: unknown) => {
+  if (typeof value !== "string") {
+    throw new Error(message + " が指定されていない!");
+  }
+  return value;
+};
 
-export const VERCEL_GIT_COMMIT_SHA = process.env
-  .VERCEL_GIT_COMMIT_SHA as string;
+export const FAUNA_SERVER_KEY = expectString(
+  "FAUNA_SERVER_KEY",
+  process.env.FAUNA_SERVER_KEY
+);
 
-export const GOOGLE_LOGIN_CLIENT_SECRET = process.env
-  .GOOGLE_LOGIN_CLIENT_SECRET as string;
+export const VERCEL_GIT_COMMIT_SHA = expectString(
+  "VERCEL_GIT_COMMIT_SHA",
+  process.env.VERCEL_GIT_COMMIT_SHA
+);
+
+export const GOOGLE_LOGIN_CLIENT_SECRET = expectString(
+  "GOOGLE_LOGIN_CLIENT_SECRET",
+  process.env.GOOGLE_LOGIN_CLIENT_SECRET
+);
 
 export const isProduction: boolean = process.env.NODE_ENV === "production";
