9721775b91a0b170ec71fc540b0b3fa2344fa562
narumincho <16481886+narumincho@users.noreply.github.com>
2022-09-10 20:51:29 +0900

    definy RPC client の作成

---
+++ definy-rpc/client/src/main.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+// eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <div>React が無事動いたってわけ</div>
+  </React.StrictMode>
+);
