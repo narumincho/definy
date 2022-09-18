7145d33e77ee98ea17976ba0c4d6077e76a01738
narumincho <16481886+narumincho@users.noreply.github.com>
2022-09-18 10:16:13 +0900

    definy RPC 入力が不要な API を呼び出せるように

--- definy-rpc/client/src/App.tsx
+++ definy-rpc/client/src/App.tsx
@@ -1,15 +1,14 @@
 import * as React from "react";
 import * as env from "./env";
+import { Button } from "../../../client/ui/Button";
+import { DetailView } from "./DetailView";
+import { FuncDetail } from "./FuncDetail";
 import { Select } from "./Select";
-import { OneLineTextEditor } from "../../../client/ui/OneLineTextEditor";
+import { ServerOrigin } from "./ServerOrigin";
 
 export const App = (): React.ReactElement => {
   const [functionList, setFunctionList] = React.useState<
-    | ReadonlyArray<{
-        readonly name: string;
-        readonly description: string;
-      }>
-    | undefined
+    ReadonlyArray<FuncDetail> | undefined
   >(undefined);
   const [selectedFunc, setSelectedFunc] = React.useState<string | undefined>(
     undefined
@@ -17,6 +16,9 @@ export const App = (): React.ReactElement => {
   const [serverName, setServerName] = React.useState<string | undefined>();
   const [serverOrigin, setServerOrigin] = React.useState<string>(
     env.serverOrigin
+  );
+  const [runResponse, setRunResponse] = React.useState<string | undefined>(
+    undefined
   );
 
   React.useEffect(() => {
@@ -26,17 +28,10 @@ export const App = (): React.ReactElement => {
       .then((response) => {
         return response.json();
       })
-      .then(
-        (
-          json: ReadonlyArray<{
-            readonly name: ReadonlyArray<string>;
-            readonly description: string;
-          }>
-        ) => {
-          setFunctionList(json.map((e) => ({ ...e, name: e.name.join(".") })));
-          setSelectedFunc(json[0]?.name.join("."));
-        }
-      )
+      .then((json: ReadonlyArray<FuncDetail>) => {
+        setFunctionList(json);
+        setSelectedFunc(json[0]?.name.join("."));
+      })
       .catch(() => {
         setFunctionList(undefined);
         setSelectedFunc(undefined);
@@ -59,6 +54,9 @@ export const App = (): React.ReactElement => {
       });
   }, [serverOrigin]);
 
+  const selectedFuncDetail = functionList?.find(
+    (func) => func.name.join(".") === selectedFunc
+  );
   return (
     <div
       css={{
@@ -83,43 +81,16 @@ export const App = (): React.ReactElement => {
         definy RPC Browser Client
       </h2>
 
-      <div css={{ padding: 16 }}>
-        <div>
-          <label
-            css={{
-              display: "grid",
-              gridAutoFlow: "column",
-              gap: 16,
-              justifyContent: "start",
-            }}
-          >
-            server origin
-            <OneLineTextEditor
-              id="server-origin"
-              value={serverOrigin}
-              css={{
-                fontFamily: "monospace",
-              }}
-              onChange={(value) => {
-                setServerOrigin(value);
-              }}
-            ></OneLineTextEditor>
-          </label>
-        </div>
-        <h1
-          css={{
-            margin: 0,
-            backgroundColor: serverName === undefined ? "#444" : "transparent",
-          }}
-        >
-          {serverName === undefined ? "..." : serverName}
-        </h1>
-      </div>
+      <ServerOrigin
+        serverName={serverName}
+        serverOrigin={serverOrigin}
+        onChangeServerOrigin={setServerOrigin}
+      />
 
       <div
         css={{ padding: 16, display: "grid", gridTemplateColumns: "1fr 1fr" }}
       >
-        <div>
+        <div css={{ display: "grid", alignContent: "start" }}>
           <Select
             values={functionList}
             value={selectedFunc}
@@ -127,8 +98,32 @@ export const App = (): React.ReactElement => {
               setSelectedFunc(e);
             }}
           />
-          <button>Run まだ..</button>
+          <Button
+            onClick={
+              selectedFuncDetail?.input.fullName.join(".") === "definyRpc.Unit"
+                ? () => {
+                    const url = new URL(serverOrigin);
+                    url.pathname = "/" + selectedFuncDetail.name.join("/");
+                    fetch(url)
+                      .then((response) => {
+                        return response.json();
+                      })
+                      .then((json: unknown) => {
+                        console.log("response", json);
+                        setRunResponse(JSON.stringify(json));
+                      })
+                      .catch(() => {
+                        setRunResponse(undefined);
+                      });
+                  }
+                : undefined
+            }
+          >
+            Run
+          </Button>
+          {runResponse}
         </div>
+
         {functionList === undefined ? (
           <div>loading...</div>
         ) : (
@@ -138,39 +133,6 @@ export const App = (): React.ReactElement => {
           />
         )}
       </div>
-    </div>
-  );
-};
-
-const DetailView = (props: {
-  readonly functionList: ReadonlyArray<{
-    readonly name: string;
-    readonly description: string;
-  }>;
-  readonly selectedFuncName: string | undefined;
-}): React.ReactElement => {
-  if (props.selectedFuncName === undefined) {
-    return (
-      <div>
-        <h2>未選択</h2>
-      </div>
-    );
-  }
-  const selectedFuncDetail = props.functionList.find(
-    (func) => func.name === props.selectedFuncName
-  );
-
-  if (selectedFuncDetail === undefined) {
-    return (
-      <div>
-        <h2>不明な関数</h2>
-      </div>
-    );
-  }
-  return (
-    <div>
-      <h2>{selectedFuncDetail.name}</h2>
-      <div>{selectedFuncDetail.description}</div>
     </div>
   );
 };
---
+++ definy-rpc/client/src/DetailView.tsx
@@ -0,0 +1,33 @@
+import * as React from "react";
+import { FuncDetail } from "./FuncDetail";
+
+export const DetailView = (props: {
+  readonly functionList: ReadonlyArray<FuncDetail>;
+  readonly selectedFuncName: string | undefined;
+}): React.ReactElement => {
+  if (props.selectedFuncName === undefined) {
+    return (
+      <div>
+        <h2>未選択</h2>
+      </div>
+    );
+  }
+  const selectedFuncDetail = props.functionList.find(
+    (func) => func.name.join(".") === props.selectedFuncName
+  );
+
+  if (selectedFuncDetail === undefined) {
+    return (
+      <div>
+        <h2>不明な関数</h2>
+      </div>
+    );
+  }
+  return (
+    <div>
+      <h2>{selectedFuncDetail.name}</h2>
+      <div>{selectedFuncDetail.description}</div>
+      <div>{JSON.stringify(selectedFuncDetail.input)}</div>
+    </div>
+  );
+};
