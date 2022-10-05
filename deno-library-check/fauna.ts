9f31c01f2132ea217f1f8561251981e0c3deba0d
narumincho <16481886+narumincho@users.noreply.github.com>
2022-10-05 15:37:20 +0900

    deno で fauna cleint が動くことを確認

---
+++ deno-library-check/fauna.ts
@@ -0,0 +1,11 @@
+import faunadb from "https://cdn.skypack.dev/faunadb@4.7.0?dts";
+import { faunaServerKey } from "../databaseMigrationSecret.ts";
+
+const client = new faunadb.Client({
+  secret: faunaServerKey,
+  domain: "db.us.fauna.com",
+});
+
+const result = await client.query(faunadb.Paginate(faunadb.Collections()));
+
+console.log(result);
