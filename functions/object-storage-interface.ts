c654ad0cda74d75a769f26fd3ae3d9a46243b900
narumincho <16481886+narumincho@users.noreply.github.com>
2022-10-09 11:49:28 +0900

    Cloud Storage → cloudflare r2 に変更

---
+++ functions/object-storage-interface.ts
@@ -0,0 +1,62 @@
+import * as crypto from "node:crypto";
+import * as zodType from "../common/zodType";
+import { CLOUDFLARE_R2_SECRET_KEY } from "./environmentVariables";
+import { Readable } from "node:stream";
+import {
+  S3Client,
+  PutObjectCommand,
+  GetObjectCommand,
+} from "@aws-sdk/client-s3";
+
+/**
+ * https://dash.cloudflare.com/6a8354084cc02bb1c5f9ca1bb3442704/r2/
+ */
+const storageClient = new S3Client({
+  region: "auto",
+  endpoint: "https://6a8354084cc02bb1c5f9ca1bb3442704.r2.cloudflarestorage.com",
+  credentials: {
+    accessKeyId: "1a720ab590dcdc06eb0be70fd690ff39",
+    secretAccessKey: CLOUDFLARE_R2_SECRET_KEY,
+  },
+});
+
+const bucketName = "definy";
+
+/**
+ * Cloud Storage に PNGファイルを保存する.
+ * ファイル名はファイルの中身のハッシュ値
+ */
+export const savePngFile = async (
+  binary: Uint8Array
+): Promise<zodType.ImageHash> => {
+  const hash = createImageTokenFromUint8ArrayAndMimeType(binary);
+  await storageClient.send(
+    new PutObjectCommand({
+      Bucket: bucketName,
+      Key: hash,
+      Body: binary,
+      ContentType: "image/png",
+    })
+  );
+  return hash;
+};
+
+export const getPngFileReadable = async (
+  hash: zodType.ImageHash
+): Promise<Readable> => {
+  const response = await storageClient.send(
+    new GetObjectCommand({ Bucket: "definy", Key: hash })
+  );
+  return response.Body as Readable;
+};
+
+export const createImageTokenFromUint8ArrayAndMimeType = (
+  binary: Uint8Array
+): zodType.ImageHash =>
+  zodType.ImageHash.parse(
+    crypto
+      .createHash("sha256")
+      .update(binary)
+      .update("image/png", "utf8")
+      .digest("hex")
+  );
