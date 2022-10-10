abf40752cb8360309143f44c50a4ca5a0663b26b
narumincho <16481886+narumincho@users.noreply.github.com>
2022-10-11 08:55:54 +0900

    core/main 重複コード削除

--- core/main.ts
+++ core/main.ts
@@ -9,35 +9,6 @@ import { exprToDebugString } from "./toDebugString";
 import { jsTs } from "../deno-lib/npm";
 import { typePartMapToVariable } from "./variable/main";
 
-export const stringToValidUserName = (userName: string): string | null => {
-  const normalized = normalizeOneLineString(userName);
-  const { length } = [...normalized];
-  if (length <= 0 || length > 50) {
-    return null;
-  }
-  return normalized;
-};
-
-export const stringToValidProjectName = (
-  projectName: string
-): string | null => {
-  const normalized = normalizeOneLineString(projectName);
-  const { length } = [...normalized];
-  if (length <= 0 || length > 50) {
-    return null;
-  }
-  return normalized;
-};
-
-export const stringToValidIdeaName = (ideaName: string): string | null => {
-  const normalized = normalizeOneLineString(ideaName);
-  const { length } = [...normalized];
-  if (length <= 0 || length > 100) {
-    return null;
-  }
-  return normalized;
-};
-
 export const stringToValidComment = (comment: string): string | null => {
   const normalized = normalizeMultiLineString(comment);
   const { length } = [...normalized];
@@ -62,29 +33,6 @@ const normalizeMultiLineString = (text: string): string => {
         codePoint > 0xa0)
     ) {
       result += char;
-    }
-  }
-  return result;
-};
-
-/**
- * NFKCで正規化して, 先頭末尾の空白をなくし, 空白の連続を1つの空白にまとめ, 改行を取り除く
- */
-export const normalizeOneLineString = (text: string): string => {
-  const normalized = text.normalize("NFKC").trim();
-  let result = "";
-  let beforeSpace = false;
-  for (const char of normalized) {
-    const codePoint = char.codePointAt(0);
-    // 制御文字
-    if (
-      codePoint !== undefined &&
-      ((codePoint > 0x1f && codePoint < 0x7f) || codePoint > 0xa0)
-    ) {
-      if (!(beforeSpace && char === " ")) {
-        result += char;
-        beforeSpace = char === " ";
-      }
     }
   }
   return result;
