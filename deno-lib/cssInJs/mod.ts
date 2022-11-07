910e28b255b2f19c4a89e9a5efa250ed8d9de587
narumincho <16481886+narumincho@users.noreply.github.com>
2022-11-07 12:30:55 +0900

    cssInJs を改良

--- deno-lib/cssInJs/mod.ts
+++ deno-lib/cssInJs/mod.ts
@@ -7,40 +7,89 @@ export type Style = {
   readonly cursor?: "pointer" | "not-allowed" | undefined;
   readonly border?: "none" | undefined;
   readonly padding?: number | undefined;
-  readonly "text-align"?: "left" | undefined;
-  readonly "font-size"?: number | undefined;
-  readonly "background-color"?: string | undefined;
+  readonly textAlign?: "left" | undefined;
+  readonly fontSize?: number | undefined;
+  readonly backgroundColor?: string | undefined;
   readonly color?: string | undefined;
-  readonly "border-radius"?: number | undefined;
+  readonly borderRadius?: number | undefined;
 };
+
+const stylePropertyNameCamelCaseToKebabCaseMap: ReadonlyMap<string, string> =
+  new Map<keyof Style, string>([
+    ["textAlign", "text-align"],
+    ["fontSize", "font-size"],
+    ["backgroundColor", "background-color"],
+    ["borderRadius", "border-radius"],
+  ]);
 
 export type StateStyle = {
   readonly hover: Style;
   readonly disabled: Style;
 };
 
-const insertedStyle = new Map<string, Style>();
+export type StyleAndHash = {
+  readonly style: Style;
+  readonly stateStyle: StateStyle;
+  readonly hash: string;
+};
+
+export const resetInsertedStyle = (): void => {
+  insertedStyle.clear();
+};
+
+/**
+ * すでに追加したスタイルの辞書
+ *
+ * SSRのために, スタイル本体まで保持している
+ */
+const insertedStyle = new Map<
+  string,
+  { readonly style: Style; readonly state: StateStyle }
+>();
+
+/**
+ * スタイルのハッシュ値を計算する. c に渡すことによって `className` を得ることができる
+ *
+ * コンポーネントの外で使うとパフォーマンスが良くなるが, 動的な場合は中で使うことができる
+ */
+export const toStyleAndHash = (
+  style: Style,
+  state?: Partial<StateStyle>
+): StyleAndHash => {
+  const stateStyle: StateStyle = {
+    hover: state?.hover ?? {},
+    disabled: state?.disabled ?? {},
+  };
+  const hash = hashStyle(style, stateStyle);
+  return {
+    style,
+    stateStyle,
+    hash,
+  };
+};
 
 /**
  * CSS からクラス名を生成する
  *
  * Deno で esbuild でバンドルしたときに styled-component, emotion など動かなかっため自作した
  */
-export const css = (style: Style, state?: Partial<StateStyle>): string => {
-  const stateStyle: StateStyle = {
-    hover: state?.hover ?? {},
-    disabled: state?.disabled ?? {},
-  };
-  const hash = hashStyle(style, stateStyle);
-  const className = "d_" + hash;
-  if (insertedStyle.has(hash)) {
+export const c = (styleAndHash: StyleAndHash): string => {
+  const className = "d_" + styleAndHash.hash;
+  if (insertedStyle.has(styleAndHash.hash)) {
     return className;
   }
-  insertedStyle.set(hash, style);
+  insertedStyle.set(styleAndHash.hash, {
+    style: styleAndHash.style,
+    state: styleAndHash.stateStyle,
+  });
   if (globalThis.document === undefined) {
     return className;
   }
-  const cssString = styleAndStateStyleToCssString(className, style, stateStyle);
+  const cssString = styleAndStateStyleToCssString(
+    className,
+    styleAndHash.style,
+    styleAndHash.stateStyle
+  );
   console.log(cssString);
   const styleElement = document.getElementById(definyCssInJsId);
   if (styleElement instanceof HTMLStyleElement) {
@@ -79,10 +128,17 @@ const styleToCssString = (selector: string, style: Style): string => {
     selector +
     " {\n" +
     declarations
-      .map(
-        ([k, v]) =>
-          k + ":" + (typeof v === "number" && v !== 0 ? v + "px" : v) + ";"
-      )
+      .map(([key, value]) => {
+        if (value === undefined) {
+          return "";
+        }
+        return (
+          (stylePropertyNameCamelCaseToKebabCaseMap.get(key) ?? key) +
+          ":" +
+          (typeof value === "number" && value !== 0 ? value + "px" : value) +
+          ";"
+        );
+      })
       .join("\n") +
     "\n}"
   );
