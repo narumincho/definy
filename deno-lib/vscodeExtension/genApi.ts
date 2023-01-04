6a0bd0657c265e70d405b68e13f7adf6f22d970a
narumincho <16481886+narumincho@users.noreply.github.com>
2023-01-04 14:46:57 +0900

    vscode 拡張機能版を deno でデプロイ?

---
+++ deno-lib/vscodeExtension/genApi.ts
@@ -0,0 +1,7 @@
+export const generate = async (): Promise<string> => {
+  const code = await (await fetch(
+    "https://raw.githubusercontent.com/DefinitelyTyped/DefinitelyTyped/master/types/vscode/index.d.ts",
+  )).text();
+  // ast を組み立てる?
+  // return code.replace("declare module 'vscode' {", "");
+};
