8a39b8b8ac1e3101642843f104efd4b34bf38569
narumincho <16481886+narumincho@users.noreply.github.com>
2024-01-15 14:03:26 +0900

    use createRegExpType

--- server/type/id.ts
+++ server/type/id.ts
@@ -1,17 +1,23 @@
-import { createIdGraphQLScalarType } from "npm:@narumincho/simple-graphql-server-common@0.1.2";
+import { createRegExpType } from "npm:@narumincho/simple-graphql-server-common@0.1.2";
+
+const idRegExp = /^[0-9a-f]{32}$/;
 
 export type AccountId = string & { readonly AccountId: unique symbol };
 
-export const AccountId = createIdGraphQLScalarType<AccountId>(
-  "AccountId",
-);
+export const AccountId = createRegExpType<AccountId>({
+  name: "AccountId",
+  description: "",
+  regexp: idRegExp,
+});
 
 export const accountIdFrom = (id: string): AccountId => id as AccountId;
 
 export type TotpKeyId = string & { readonly TotpKeyId: unique symbol };
 
-export const TotpKeyId = createIdGraphQLScalarType<TotpKeyId>(
-  "TotpKeyId",
-);
+export const TotpKeyId = createRegExpType<TotpKeyId>({
+  name: "TotpKeyId",
+  description: "",
+  regexp: idRegExp,
+});
 
 export const totpKeyIdIdFrom = (id: string): TotpKeyId => id as TotpKeyId;
